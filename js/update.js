f268e3ab9d615e90972c476f4348a2241312aece
lhshilin <74134634+lhshilin@users.noreply.github.com>
2023-01-30 23:13:38 +0800

    Add files via upload

---
+++ js/update.js
@@ -0,0 +1,41 @@
+$(function () {
+    // 当前版本信息写入
+    var version = '2023.01.31.0',
+        sourceCodeUrl = 'https://lhshilin.github.io/jimu/download/积木' + version + '版本源码.zip',
+        androidUrl = 'https://lhshilin.github.io/jimu/download/积木_' + version + '.apk';
+    $('#sourceCodeDownload').attr('data-clipboard-text', sourceCodeUrl)
+    $('#sourceCode a').attr({
+        href : sourceCodeUrl,
+        download : '积木' + version + '版本源码.zip'
+    })
+    $('#androidDownload').attr('data-clipboard-text', androidUrl)
+    $('#android a').attr({
+        href : androidUrl,
+        download : '积木_' + version + '.apk'
+    })
+    // 下次不提示更新本地存储数据
+    if(!localStorage.getItem('updateTip')) {
+        localStorage.setItem('updateTip', 'true')
+    }
+    // 判断最新版本
+    $.get('https://lhshilin.github.io/jimu/update.json', function (res) {
+        if(version === res.updateV) {
+            console.log('已经是最新版本')
+        }else if(localStorage.getItem('updateTip') == 'true' || localStorage.getItem('updateV') !== res.updateV) {
+            $('.update').stop().fadeIn(1500)
+            $('.cover').show()
+            $('body').css('overflow', 'hidden')
+        }
+        $('.update .androidUrl a').prop('href', res.androidUrl).children('span').html(res.androidUrl)
+        $('.update .updatebtnleft').on('click', function () {
+            $('.update').stop().fadeOut(300)
+            $('.cover').hide()
+            $('body').css('overflow', 'visible')
+            localStorage.setItem('updateTip', 'false')
+            localStorage.setItem('updateV', res.updateV)
+        })
+        $('.update .updatebtnright').on('click', function () {
+            window.location.href = res.androidUrl;
+        })
+    });
+})
