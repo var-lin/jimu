819b7329a692e4fdc0d53c22d4ef144aca717853
lhshilin <74134634+lhshilin@users.noreply.github.com>
2023-01-30 23:14:49 +0800

    Add files via upload

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 $(function () {
     // 初始化 显示
-    $('#homepage').stop().fadeIn(300)
-    $('footer button .text').addClass('texttype')
+    $('#homepage').stop().fadeIn(300);
+    $('footer button .text').addClass('texttype');
     // 功能链接数据加载
     $.get('https://lhshilin.github.io/jimu/allFunctionData.json', function (res) {
         var fnnum = 0;
         // 数据载入
         // QQ专区数据输入及输入进全部功能里
         $(res.QQ).each(function (i, e) {
-            $('#homepage .fn-qq p').append('<a class="col-4"></a>').children('a').eq(i).html(e.name).prop('href', e.url)
+            $('#homepage .fn-qq p').append('<a class="col-4""></a>').children('a').eq(i).html(e.name).prop('href', e.url)
         })
         fnnum += res.QQ.length;
         //  信息查询专区数据输入及输入进全部功能里
@@ -33,7 +33,9 @@ $(function () {
         fnnum += res.other.length;
         // 输出一共的功能数量
         $('#homepage .func-num span').html(fnnum)
-    })
+
+        mysteriouCode(res)
+    });
     // 更新日志数据输入
     $.get('https://lhshilin.github.io/jimu/log.json', function (res) {
         $(res).each(function (i, v) {
@@ -42,20 +44,20 @@ $(function () {
                 $('#toupdate > ul > li > ul').eq(i).append('<li></li>').children('li').eq(index).html(value)
             })          
         })
-    })
-    // 获得软件已运行时间
-    function pastTime() {       
-        var pasttimeevents = $('#homepage .pasttime span')
+    });
+    // 获得软件已运行时间 pastTime
+    (function () {       
+        var pasttimeevents = $('#homepage .pasttime span'),
         // 设置发布时间
-        var old_time = new Date(2022, 0, 16)          
-           var new_time,         
+            old_time = new Date(2022, 0, 16),
+            new_time,         
             day, 
             leave_day, 
             hour, 
             leave_hour, 
             minute,
             second;
-           function caleTimeDiff() {
+        function caleTimeDiff() {
             // 获取最新时间
             new_time = new Date()         
             // 计算时间差(差多少秒)
@@ -68,33 +70,31 @@ $(function () {
             second = Math.floor(leave_hour % 60);        
             // 时间输出
             pasttimeevents.eq(0).html(day)
-               pasttimeevents.eq(1).html(hour)
+            pasttimeevents.eq(1).html(hour)
             pasttimeevents.eq(2).html(minute)
-             pasttimeevents.eq(3).html(second)
+            pasttimeevents.eq(3).html(second)
         }
         caleTimeDiff()
-           setInterval(caleTimeDiff, 999)
-      }
-    pastTime()
-    // 更新日志返回置顶显示隐藏功能
-    function toupDateReturnTop() {
+        setInterval(caleTimeDiff, 999)
+    }());
+    // 更新日志返回置顶显示隐藏功能 toupDateReturnTop
+    (function () {
         $(window).on('scroll', function () {
             var htmlheight = $(window).height() / 2;
             if($('html').scrollTop() > htmlheight && $('#toupdate').css('display') != 'none') {
-                   $('#toupdate #returntop').show()
+                $('#toupdate #returntop').show()
             }else {
                 $('#toupdate #returntop').hide()
             }
         })
         $('#toupdate #returntop').on('click', function () {
             $('html').stop().animate({
-                   scrollTop : 0
-               })
+                scrollTop : 0
+            })
         })
-    }
-    toupDateReturnTop()
-    // 底部按钮切换功能 
-    function footerBtnType() {
+    }());
+    // 底部按钮切换功能 footerBtnType
+    (function () {
         $('footer button').each(function (i, e) {
             $(this).click(function () {
                    // 点击底部按钮返回滚动条置顶
@@ -103,35 +103,34 @@ $(function () {
                 $('#showregion > div').eq(i).stop().fadeIn(300).siblings().hide()
                 // 文字效果切换
                   $(e).find('.text span').css({
-                       fontSize : '.6rem',
+                    fontSize : '.6rem',
                     color : '#fedcba',
-                       backgroundColor : 'cornflowerblue'
+                    backgroundColor : 'cornflowerblue'
                 })
                 $(e).siblings().find('.text span').css({
                     fontSize : '.5rem',
                     color : '#fff',
-                       backgroundColor : 'transparent'
+                    backgroundColor : 'transparent'
                 })
             })
         })
-    }
-    footerBtnType()    
+    }());
     // 打赏二维码切换效果
     $('#reward button').eq(0).click(function () {
         $(this).addClass('btn-success').siblings('button').removeClass('btn-primary')
         $('#reward img').eq(0).stop().fadeIn(500).siblings('img').hide()
-    })
+    });
     $('#reward button').eq(1).click(function () {
         $(this).addClass('btn-primary').siblings('button').removeClass('btn-success')
         $('#reward img').eq(1).stop().fadeIn(500).siblings('img').hide()
-    })
-    // 点击复制功能
-    function copy() {       
+    });
+    // 点击复制功能 copy
+    (function () {       
         // QQ复制按钮
         var authorQQcopybtn = new Clipboard('#authorQQcopybtn'),
             authorQQmailcopybtn = new Clipboard('#authorQQmailcopybtn'),
             authorQQCommunicationGroup = new Clipboard('#authorQQCommunicationGroup'),
-            aliyunpanDownload = new Clipboard('#aliyunpanDownload'),
+            sourceCodeDownload = new Clipboard('#sourceCodeDownload'),
             androidDownload = new Clipboard('#androidDownload'),
             lanzouyunDownload = new Clipboard('#lanzouyunDownload');
         // QQ号复制按钮
@@ -159,12 +158,12 @@ $(function () {
             $('#authorQQCommunicationGroup').html('复制失败')
         })
         // pc端下载链接点击复制
-           aliyunpanDownload.on('success', function(e) {
+        sourceCodeDownload.on('success', function(e) {
             e.clearSelection();
-            $('#aliyunpanDownload').html('复制成功')
+            $('#sourceCodeDownload').html('复制成功')
         })
-        aliyunpanDownload.on('error', function(e) {
-            $('#aliyunpanDownload').html('复制失败')
+        sourceCodeDownload.on('error', function(e) {
+            $('#sourceCodeDownload').html('复制失败')
         })
         // 安卓版下载链接点击复制
         androidDownload.on('success', function(e) {
@@ -182,10 +181,92 @@ $(function () {
         lanzouyunDownload.on('error', function(e) {
             $('#lanzouyunDownload').html('复制失败')
         })
-    }
-    copy()
+    }());
+    // mysteriouCode
+    function mysteriouCode(res) {
+        function mysteriouCodeUrl() {
+            // $(res.mysteriouCode).each(function (i, e) {
+            //     $('#homepage .fn-mysteriouCode .row').append('<a class="col-4"></a>').children('a').eq(i).html(e.name).prop('href', e.url)
+            // })
+            // // 输出一共的功能数量
+            // var funcNum = parseInt($('#homepage .func-num span').html())
+            // $('#homepage .func-num span').html(funcNum + res.mysteriouCode.length)
+            $('.fn-mysteriouCode').show()
+        }
+        if(!localStorage.getItem('mysteriouCode')) {
+            localStorage.setItem('mysteriouCode', 'false')
+        }
+        if(localStorage.getItem('mysteriouCode') === 'true') {
+            mysteriouCodeUrl()
+        } else {
+            var count = 0,
+                timer;
+            function down() {
+                timer = setInterval(function () {
+                    count ++;
+                    if(count == 3) {
+                        count = 0;
+                        clearInterval(timer)
+                        $('.mysteriouCode').fadeIn(300)
+                        $('.cover').show()
+                        $('body').css('overflow', 'hidden')
+                        $(window).on('keydown', function (e) {
+                            if(e.keyCode === 13) {
+                                $('.mysteriouCode .mysteriouCodeRight').click()
+                                $(window).on('keydown', null)
+                            }
+                        })
+                    }
+                }, 1000)
+                return timer
+            }
+            function clear() {
+                $('.mysteriouCode').hide(300)
+                $('.cover').hide()
+                $('body').css('overflow', 'visible')
+                $(window).on('keydown', null)
+            }
+            $('.mysteriouCode .mysteriouCodeLeft').on('click', function () {
+                clear()
+            })
+            $('.mysteriouCode .mysteriouCodeRight').on('click', function () {
+                if($('.mysteriouCode input').val() === '') {
+                    return $('.mysteriouCode .mysteriouCodeTip .meg').html('密码不能为空')
+                }
+                $.get('https://v.api.aa1.cn/api/api-md5/go.php', {act : '加密', md5 : $('.mysteriouCode input').val()}, function (res) {
+                    res = res.slice(6, 38)
+                    if(res == '871925f4b0a5cd9e35cd0340edea0e82') {
+                        $('.mysteriouCode .mysteriouCodeTip .meg').html('密码正确')
+                        mysteriouCodeUrl()
+                        localStorage.setItem('mysteriouCode', 'true')
+                        clear()
+                    } else {
+                        $('.mysteriouCode .mysteriouCodeTip .meg').html('密码错误')
+                    }
+                })
+            })
+            $('#logo').on({
+                mousedown : function () {
+                    down()
+                    $('#logo').on('mouseup', function () {
+                        count = 0;
+                        clearInterval(timer)
+                        return false;
+                    }) 
+                },
+                touchstart : function () {
+                    down()
+                    $('#logo').on('touchend', function () {
+                        count = 0;
+                        clearInterval(timer)
+                        return false;
+                    }) 
+                }
+            })
+        }
+    };
     // 下雪特效执行
     $(document).ready( function() {
-           $("canvas.xiaxue").let_it_snow();
-    })
+        $("canvas.xiaxue").let_it_snow();
+    });
 })
